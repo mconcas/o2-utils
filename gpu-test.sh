877669fef22cd3595e29bdf307006bfab5cf3866
Matteo Concas <mconcas@cern.ch>
2019-03-25 18:33:59 +0000

    Add limited range exploration for steer macro

--- gpu-test.sh
+++ gpu-test.sh
@@ -1,16 +1,23 @@
 #! /usr/bin/bash -ex
 
 if [[ -z "$1" ]];
- then
- evt=
-else
- evt=$1
+  then
+    evt=
+  else
+    startfrom=$1
+fi
+
+if [[ -z "$2" ]];
+  then
+    endat=
+  else
+    endat=",$2"
 fi
 
 export ALIBUILD_WORK_DIR="/home/alidock/alice/sw"
 eval $(alienv load O2/latest 2> /dev/null)
 
 pushd ~/alice/data/000vtx-alien/
-cuda-memcheck root -l -q run_vert_gpu.C++\($evt\)
+cuda-memcheck root -l -q run_vert_gpu.C++\($startfrom$endat\)
 echo test returned $?
 popd
