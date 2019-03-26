efd0fe73a4ce7ad5115e9b0d293b1e72e637687a
Matteo Concas <mconcas@cern.ch>
2019-03-26 10:21:37 +0100

    Improve gpu-test interface

--- gpu-test.sh
+++ gpu-test.sh
@@ -1,23 +1,28 @@
 #! /usr/bin/bash -ex
 
 if [[ -z "$1" ]];
+then
+  mcvalid=
+else
+  mcvalid=true
+  if [[ -z "$2" ]];
   then
     evt=
   else
-    startfrom=$1
-fi
-
-if [[ -z "$2" ]];
-  then
-    endat=
-  else
-    endat=",$2"
+    startfrom=",$2"
+    if [[ -z "$3" ]];
+    then
+      endat=
+    else
+      endat=",$3"
+    fi
+  fi
 fi
 
 export ALIBUILD_WORK_DIR="/home/alidock/alice/sw"
 eval $(alienv load O2/latest 2> /dev/null)
 
 pushd ~/alice/data/000vtx-alien/
-cuda-memcheck root -l -q run_vert_gpu.C++\($startfrom$endat\)
+cuda-memcheck root -l -q run_vert_gpu.C++\($mcvalid$startfrom$endat\)
 echo test returned $?
 popd
