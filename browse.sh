31a52d071a72779d5e7b6cc5bfce020f98150665
Matteo Concas <mconcas@cern.ch>
2019-04-04 15:21:55 +0200

    Improve browse macro

--- browse.sh
+++ browse.sh
@@ -1,9 +1,9 @@
-#! /usr/bin/bash -ex
+#! /usr/bin/bash -e
 
 export ALIBUILD_WORK_DIR="/home/alidock/alice/sw"
 eval $(alienv load O2/latest 2> /dev/null)
 
 # pushd ~/alice/data/000vtx-alien/
-root -l /home/alidock/util/VertexerDataBrowser.C++
+root -l /home/alidock/util/VertexerDataBrowser.C++\("\"/home/alidock/alice/data/000vtx-alien/vertexer_${1:?chose either serial or gpu}_data.root"\"\)
 echo test returned $?
 # popd
