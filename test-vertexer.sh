74421e912143a9da71929f2b56b232f01fdc55c6
Matteo Concas <mconcas@cern.ch>
2019-06-20 12:05:26 +0200

    Cleanup stale files and trash

--- test-vertexer.sh
+++ test-vertexer.sh
@@ -3,7 +3,7 @@
 export ALIBUILD_WORK_DIR="/home/alidock/alice/sw"
 eval $(alienv load O2/latest 2> /dev/null)
 
-pushd ~/alice/data/000vtx-alien/
-root -l -q run_primary_vertexer_ITS.C++\("${1:-false},${2:-false},${3:--1},${4:-1}"\)
+pushd /mnt/PythiaPbPb/MB/001/
+root -l -q /home/alidock/alice/O2/macro/run_primary_vertexer_ITS.C++\("${1:-false},${2:-false},${3:--1},${4:-1}"\)
 echo test returned $?
 popd
