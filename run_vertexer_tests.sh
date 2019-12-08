5a4611d9ff149f60bc5753c732bee13b9a16197d
Matteo Concas <mconcas@cern.ch>
2019-12-08 19:28:29 +0100

    Vary deltaphi with large deltatanlambdai

--- run_vertexer_tests.sh
+++ run_vertexer_tests.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-# cd /data1/ruben/pbpbVtx
 
+# Default deltatanlambda varies phi
+
+# cd /data1/ruben/pbpbVtx
 # root -l -b -q run_primary_vertexer_ITS.C++\(0.005,GPUDataTypes::DeviceType::CPU,false,20,1\) && mv dbg_ITSVertexerCPU.root phiCutVariationData/dbg_ITSVertexerCPU_PhiCut_0005.root
 # root -l -b -q run_primary_vertexer_ITS.C++\(0.010,GPUDataTypes::DeviceType::CPU,false,20,1\) && mv dbg_ITSVertexerCPU.root phiCutVariationData/dbg_ITSVertexerCPU_PhiCut_001.root
 # root -l -b -q run_primary_vertexer_ITS.C++\(0.020,GPUDataTypes::DeviceType::CPU,false,20,1\) && mv dbg_ITSVertexerCPU.root phiCutVariationData/dbg_ITSVertexerCPU_PhiCut_002.root
@@ -12,21 +14,34 @@
 # root -l -b -q run_primary_vertexer_ITS.C++\(0.080,GPUDataTypes::DeviceType::CPU,false,20,1\) && mv dbg_ITSVertexerCPU.root phiCutVariationData/dbg_ITSVertexerCPU_PhiCut_008.root
 # root -l -b -q run_primary_vertexer_ITS.C++\(0.090,GPUDataTypes::DeviceType::CPU,false,20,1\) && mv dbg_ITSVertexerCPU.root phiCutVariationData/dbg_ITSVertexerCPU_PhiCut_009.root
 # root -l -b -q run_primary_vertexer_ITS.C++\(0.100,GPUDataTypes::DeviceType::CPU,false,20,1\) && mv dbg_ITSVertexerCPU.root phiCutVariationData/dbg_ITSVertexerCPU_PhiCut_010.root
-
 # cd -
 
+# fix phi and varies deltatanlambda
+# cd /data1/ruben/pbpbVtx
+# root -l -b -q run_primary_vertexer_ITS.C++\(0.100,0.001,GPUDataTypes::DeviceType::CPU,false\) && mv dbg_ITSVertexerCPU.root tanLambdaCutVariationData/dbg_ITSVertexerCPU_tanLambdaCut_001.root
+# root -l -b -q run_primary_vertexer_ITS.C++\(0.100,0.002,GPUDataTypes::DeviceType::CPU,false\) && mv dbg_ITSVertexerCPU.root tanLambdaCutVariationData/dbg_ITSVertexerCPU_tanLambdaCut_002.root
+# root -l -b -q run_primary_vertexer_ITS.C++\(0.100,0.003,GPUDataTypes::DeviceType::CPU,false\) && mv dbg_ITSVertexerCPU.root tanLambdaCutVariationData/dbg_ITSVertexerCPU_tanLambdaCut_003.root
+# root -l -b -q run_primary_vertexer_ITS.C++\(0.100,0.004,GPUDataTypes::DeviceType::CPU,false\) && mv dbg_ITSVertexerCPU.root tanLambdaCutVariationData/dbg_ITSVertexerCPU_tanLambdaCut_004.root
+# root -l -b -q run_primary_vertexer_ITS.C++\(0.100,0.005,GPUDataTypes::DeviceType::CPU,false\) && mv dbg_ITSVertexerCPU.root tanLambdaCutVariationData/dbg_ITSVertexerCPU_tanLambdaCut_005.root
+# root -l -b -q run_primary_vertexer_ITS.C++\(0.100,0.006,GPUDataTypes::DeviceType::CPU,false\) && mv dbg_ITSVertexerCPU.root tanLambdaCutVariationData/dbg_ITSVertexerCPU_tanLambdaCut_006.root
+# root -l -b -q run_primary_vertexer_ITS.C++\(0.100,0.007,GPUDataTypes::DeviceType::CPU,false\) && mv dbg_ITSVertexerCPU.root tanLambdaCutVariationData/dbg_ITSVertexerCPU_tanLambdaCut_007.root
+# root -l -b -q run_primary_vertexer_ITS.C++\(0.100,0.008,GPUDataTypes::DeviceType::CPU,false\) && mv dbg_ITSVertexerCPU.root tanLambdaCutVariationData/dbg_ITSVertexerCPU_tanLambdaCut_008.root
+# root -l -b -q run_primary_vertexer_ITS.C++\(0.100,0.009,GPUDataTypes::DeviceType::CPU,false\) && mv dbg_ITSVertexerCPU.root tanLambdaCutVariationData/dbg_ITSVertexerCPU_tanLambdaCut_009.root
+# root -l -b -q run_primary_vertexer_ITS.C++\(0.100,0.010,GPUDataTypes::DeviceType::CPU,false\) && mv dbg_ITSVertexerCPU.root tanLambdaCutVariationData/dbg_ITSVertexerCPU_tanLambdaCut_010.root
+# root -l -b -q run_primary_vertexer_ITS.C++\(0.100,0.011,GPUDataTypes::DeviceType::CPU,false\) && mv dbg_ITSVertexerCPU.root tanLambdaCutVariationData/dbg_ITSVertexerCPU_tanLambdaCut_011.root
+# cd -
+
+# fix deltatanlambda and varies phi
 cd /data1/ruben/pbpbVtx
-
-# # root -l -b -q run_primary_vertexer_ITS.C++\(0.100,0.001,GPUDataTypes::DeviceType::CPU,false\) && mv dbg_ITSVertexerCPU.root tanLambdaCutVariationData/dbg_ITSVertexerCPU_tanLambdaCut_001.root
-# # root -l -b -q run_primary_vertexer_ITS.C++\(0.100,0.002,GPUDataTypes::DeviceType::CPU,false\) && mv dbg_ITSVertexerCPU.root tanLambdaCutVariationData/dbg_ITSVertexerCPU_tanLambdaCut_002.root
-# # root -l -b -q run_primary_vertexer_ITS.C++\(0.100,0.003,GPUDataTypes::DeviceType::CPU,false\) && mv dbg_ITSVertexerCPU.root tanLambdaCutVariationData/dbg_ITSVertexerCPU_tanLambdaCut_003.root
-# # root -l -b -q run_primary_vertexer_ITS.C++\(0.100,0.004,GPUDataTypes::DeviceType::CPU,false\) && mv dbg_ITSVertexerCPU.root tanLambdaCutVariationData/dbg_ITSVertexerCPU_tanLambdaCut_004.root
-# # root -l -b -q run_primary_vertexer_ITS.C++\(0.100,0.005,GPUDataTypes::DeviceType::CPU,false\) && mv dbg_ITSVertexerCPU.root tanLambdaCutVariationData/dbg_ITSVertexerCPU_tanLambdaCut_005.root
-# # root -l -b -q run_primary_vertexer_ITS.C++\(0.100,0.006,GPUDataTypes::DeviceType::CPU,false\) && mv dbg_ITSVertexerCPU.root tanLambdaCutVariationData/dbg_ITSVertexerCPU_tanLambdaCut_006.root
-# # root -l -b -q run_primary_vertexer_ITS.C++\(0.100,0.007,GPUDataTypes::DeviceType::CPU,false\) && mv dbg_ITSVertexerCPU.root tanLambdaCutVariationData/dbg_ITSVertexerCPU_tanLambdaCut_007.root
-# # root -l -b -q run_primary_vertexer_ITS.C++\(0.100,0.008,GPUDataTypes::DeviceType::CPU,false\) && mv dbg_ITSVertexerCPU.root tanLambdaCutVariationData/dbg_ITSVertexerCPU_tanLambdaCut_008.root
-# # root -l -b -q run_primary_vertexer_ITS.C++\(0.100,0.009,GPUDataTypes::DeviceType::CPU,false\) && mv dbg_ITSVertexerCPU.root tanLambdaCutVariationData/dbg_ITSVertexerCPU_tanLambdaCut_009.root
-root -l -b -q run_primary_vertexer_ITS.C++\(0.100,0.010,GPUDataTypes::DeviceType::CPU,false\) && mv dbg_ITSVertexerCPU.root tanLambdaCutVariationData/dbg_ITSVertexerCPU_tanLambdaCut_010.root
-root -l -b -q run_primary_vertexer_ITS.C++\(0.100,0.011,GPUDataTypes::DeviceType::CPU,false\) && mv dbg_ITSVertexerCPU.root tanLambdaCutVariationData/dbg_ITSVertexerCPU_tanLambdaCut_011.root
-
+root -l -b -q run_primary_vertexer_ITS.C++\(0.005,0.01,GPUDataTypes::DeviceType::CPU,false\) && mv dbg_ITSVertexerCPU.root phiCutVariationLargeTanLambda/dbg_ITSVertexerCPU_PhiCut_0005.root
+root -l -b -q run_primary_vertexer_ITS.C++\(0.010,0.01,GPUDataTypes::DeviceType::CPU,false\) && mv dbg_ITSVertexerCPU.root phiCutVariationLargeTanLambda/dbg_ITSVertexerCPU_PhiCut_001.root
+root -l -b -q run_primary_vertexer_ITS.C++\(0.020,0.01,GPUDataTypes::DeviceType::CPU,false\) && mv dbg_ITSVertexerCPU.root phiCutVariationLargeTanLambda/dbg_ITSVertexerCPU_PhiCut_002.root
+root -l -b -q run_primary_vertexer_ITS.C++\(0.030,0.01,GPUDataTypes::DeviceType::CPU,false\) && mv dbg_ITSVertexerCPU.root phiCutVariationLargeTanLambda/dbg_ITSVertexerCPU_PhiCut_003.root
+root -l -b -q run_primary_vertexer_ITS.C++\(0.040,0.01,GPUDataTypes::DeviceType::CPU,false\) && mv dbg_ITSVertexerCPU.root phiCutVariationLargeTanLambda/dbg_ITSVertexerCPU_PhiCut_004.root
+root -l -b -q run_primary_vertexer_ITS.C++\(0.050,0.01,GPUDataTypes::DeviceType::CPU,false\) && mv dbg_ITSVertexerCPU.root phiCutVariationLargeTanLambda/dbg_ITSVertexerCPU_PhiCut_005.root
+root -l -b -q run_primary_vertexer_ITS.C++\(0.060,0.01,GPUDataTypes::DeviceType::CPU,false\) && mv dbg_ITSVertexerCPU.root phiCutVariationLargeTanLambda/dbg_ITSVertexerCPU_PhiCut_006.root
+root -l -b -q run_primary_vertexer_ITS.C++\(0.070,0.01,GPUDataTypes::DeviceType::CPU,false\) && mv dbg_ITSVertexerCPU.root phiCutVariationLargeTanLambda/dbg_ITSVertexerCPU_PhiCut_007.root
+root -l -b -q run_primary_vertexer_ITS.C++\(0.080,0.01,GPUDataTypes::DeviceType::CPU,false\) && mv dbg_ITSVertexerCPU.root phiCutVariationLargeTanLambda/dbg_ITSVertexerCPU_PhiCut_008.root
+root -l -b -q run_primary_vertexer_ITS.C++\(0.090,0.01,GPUDataTypes::DeviceType::CPU,false\) && mv dbg_ITSVertexerCPU.root phiCutVariationLargeTanLambda/dbg_ITSVertexerCPU_PhiCut_009.root
+root -l -b -q run_primary_vertexer_ITS.C++\(0.100,0.01,GPUDataTypes::DeviceType::CPU,false\) && mv dbg_ITSVertexerCPU.root phiCutVariationLargeTanLambda/dbg_ITSVertexerCPU_PhiCut_010.root
 cd -
